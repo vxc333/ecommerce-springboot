f3c64287417ab744084fef140b19b3ad42f87078
vxc333 <you@example.com>
2024-06-11 15:19:10 -0300

    Configuring order user relationship

--- src/main/java/com/dev/ecommerce/entities/User.java
+++ src/main/java/com/dev/ecommerce/entities/User.java
@@ -1,15 +1,26 @@
 package com.dev.ecommerce.entities;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
