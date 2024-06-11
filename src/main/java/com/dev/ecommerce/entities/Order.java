733c9156acd8961783cab30f8705f75aa315010d
vxc333 <you@example.com>
2024-06-11 15:18:33 -0300

    Creation of the Order entity and OrderStatus

---
+++ src/main/java/com/dev/ecommerce/entities/Order.java
@@ -0,0 +1,30 @@
+package com.dev.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
