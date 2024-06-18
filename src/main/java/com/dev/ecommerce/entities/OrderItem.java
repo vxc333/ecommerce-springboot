79e71113648aafa20ee090479f655c147bcc44a6
vxc333 <you@example.com>
2024-06-18 16:20:17 -0300

    Adjusting Class

--- src/main/java/com/dev/ecommerce/entities/OrderItem.java
+++ src/main/java/com/dev/ecommerce/entities/OrderItem.java
@@ -1,16 +1,40 @@
 package com.dev.ecommerce.entities;
 
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
 
+    @EmbeddedId
     private  OrderItemPK id =  new OrderItemPK();
 
     private Integer quantity;
     private Double price;
+
+    public OrderItem() {
+
+    }
 
     public OrderItem(Order order,Product product ,Double price, Integer quantity) {
         id.setOrder(order);
         id.setProduct(product);
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
     }
 }
