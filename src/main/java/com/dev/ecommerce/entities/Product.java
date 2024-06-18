59f6e2d7f33087f7013d309f9c61e1beac43697d
vxc333 <you@example.com>
2024-06-18 16:18:50 -0300

    I added OneToMany relationship

--- src/main/java/com/dev/ecommerce/entities/Product.java
+++ src/main/java/com/dev/ecommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.dev.ecommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -12,16 +13,30 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     @Column(columnDefinition = "TEXT")
     private String description;
+
     private Double price;
+
     private String imgUrl;
 
     @ManyToMany
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders(){
+        return items.stream().map(OrderItem::getOrder).toList();
+    }
 }
