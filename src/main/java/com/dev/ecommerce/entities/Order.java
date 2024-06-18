fb44481913a65263f6650afd00a7ce686dfe7ca0
vxc333 <you@example.com>
2024-06-18 16:20:42 -0300

    Creating relationship

--- src/main/java/com/dev/ecommerce/entities/Order.java
+++ src/main/java/com/dev/ecommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.dev.ecommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -20,16 +23,11 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
-    public User getClient() {
-        return client;
-    }
-
-    public void setClient(User client) {
-        this.client = client;
-    }
-
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){}
 
@@ -39,5 +37,13 @@ public class Order {
         this.status = status;
         this.client = client;
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(OrderItem::getProduct).toList();
     }
 }
